60df80f320f7841b2f0759cd509509d53e840dd0
Raghuram Elancheran <83778193+raghurame@users.noreply.github.com>
2023-03-28 02:02:05 +0530

    daily updates

--- avgBridgeCalculations/headers/computeStates.h
+++ avgBridgeCalculations/headers/computeStates.h
@@ -1,5 +1,13 @@
 #ifndef COMPUTESTATESHEUR_H
 #define COMPUTESTATESHEUR_H
 
+void printAverageStates (const char *filename_avgStates, STATES avgStates, STATES stdevStates);
+STATES computeStdevStates (STATES stdevStates, STATES avgStates, STATES *allStates, int nTimeframes);
+STATES readAllStates (STATES *allStates, int nTimeframes, const char *filename_states);
+STATES computeAvgStates (STATES avgStates, int nTimeframes);
+void printCurrentStates (FILE *file_printStates, STATES currentStates);
+STATES sumAllStates (STATES currentStates, STATES avgStates);
+STATES computeAllStates (STATES currentStates, TRAJECTORY *atoms, int nAtoms);
+STATES initializeStates (STATES currentStates);
 
 #endif
