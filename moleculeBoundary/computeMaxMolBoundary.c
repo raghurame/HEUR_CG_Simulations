9625c0bdea7c426d1be8f6134be43557a9641ea6
Raghuram <raghurame@users.noreply.github.com>
2023-05-27 11:55:09 +0530

    daily updates

--- moleculeBoundary/computeMaxMolBoundary.c
+++ moleculeBoundary/computeMaxMolBoundary.c
@@ -18,7 +18,7 @@ typedef struct coordinates
 
 typedef struct boxDimension
 {
-	float xlo, xhi, xtilt, ylo, yhi, ytilt, zlo, zhi, ztilt;
+	float xlo, xhi, xtilt, ylo, yhi, ytilt, zlo, zhi, ztilt, xLength, yLength, zLength, xLengthHalf, yLengthHalf, zLengthHalf;
 } BOX_DIMENSION;
 
 int countNAtoms (int nAtoms, const char filename[])
@@ -61,13 +61,21 @@ int main(int argc, char const *argv[])
 			currentAtom = 0;
 			fgets (lineString, 2000, inputDumpfile);
 
-			if (i == 6) {
+			if (i == 5) {
 				sscanf (lineString, "%f %f %f\n", &simDimension.xlo, &simDimension.xhi, &simDimension.xtilt); }
-			if (i == 7) {
+			if (i == 6) {
 				sscanf (lineString, "%f %f %f\n", &simDimension.ylo, &simDimension.yhi, &simDimension.ytilt); }
-			if (i == 8) {
+			if (i == 7) {
 				sscanf (lineString, "%f %f %f\n", &simDimension.zlo, &simDimension.zhi, &simDimension.ztilt); }
 		}
+
+		simDimension.xLength = simDimension.xhi - simDimension.xlo;
+		simDimension.yLength = simDimension.yhi - simDimension.ylo;
+		simDimension.zLength = simDimension.zhi - simDimension.zlo;
+
+		simDimension.xLengthHalf = simDimension.xLength / 2;
+		simDimension.yLengthHalf = simDimension.yLength / 2;
+		simDimension.zLengthHalf = simDimension.zLength / 2;
 
 		for (int i = 0; i < nAtoms; ++i)
 		{
@@ -77,27 +85,65 @@ int main(int argc, char const *argv[])
 
 		while ((currentAtom + 1) < nAtoms)
 		{
-			if (inputDumpCoords[currentAtom].type == inputDumpCoords[currentAtom + 1].type)
+			if ((inputDumpCoords[currentAtom].type == inputDumpCoords[currentAtom + 1].type) && (inputDumpCoords[currentAtom].type == 1) && (inputDumpCoords[currentAtom + 1].type == 1))
 			{
-				currentDimension.x = inputDumpCoords[currentAtom].x - inputDumpCoords[currentAtom + 1].x;
-				currentDimension.y = inputDumpCoords[currentAtom].y - inputDumpCoords[currentAtom + 1].y;
-				currentDimension.z = inputDumpCoords[currentAtom].z - inputDumpCoords[currentAtom + 1].z;
+				currentDimension.x = fabs (inputDumpCoords[currentAtom].x - inputDumpCoords[currentAtom + 1].x);
+				currentDimension.y = fabs (inputDumpCoords[currentAtom].y - inputDumpCoords[currentAtom + 1].y);
+				currentDimension.z = fabs (inputDumpCoords[currentAtom].z - inputDumpCoords[currentAtom + 1].z);
 
-				if (currentDimension.x > maxDimension.x) {
+				if (currentDimension.x > simDimension.xLengthHalf)
+				{					
+					if (inputDumpCoords[currentAtom].x > inputDumpCoords[currentAtom + 1].x) {
+						inputDumpCoords[currentAtom].x -= simDimension.xLength; }
+					else {
+						inputDumpCoords[currentAtom].x += simDimension.xLength; }
+
+					currentDimension.x = fabs (inputDumpCoords[currentAtom].x - inputDumpCoords[currentAtom + 1].x);
+				}
+
+				if (currentDimension.y > simDimension.yLengthHalf)
+				{
+					if (inputDumpCoords[currentAtom].y > inputDumpCoords[currentAtom + 1].y) {
+						inputDumpCoords[currentAtom].y -= simDimension.yLength; }
+					else {
+						inputDumpCoords[currentAtom].y += simDimension.yLength; }
+
+					currentDimension.y = fabs (inputDumpCoords[currentAtom].y - inputDumpCoords[currentAtom + 1].y);
+				}
+
+				if (currentDimension.z > simDimension.zLengthHalf)
+				{
+					if (inputDumpCoords[currentAtom].z > inputDumpCoords[currentAtom + 1].z) {
+						inputDumpCoords[currentAtom].z -= simDimension.zLength; }
+					else {
+						inputDumpCoords[currentAtom].z += simDimension.zLength; }
+
+					currentDimension.z = fabs (inputDumpCoords[currentAtom].z - inputDumpCoords[currentAtom + 1].z);
+				}
+
+				// if (currentDimension.x > 60 || currentDimension.y > 60 || currentDimension.z > 60)
+				// {
+				// 	printf("currentTimestep: %d\n", currentTimestep);
+				// 	printf("%d %d %f %f %f\n", inputDumpCoords[currentAtom].id, inputDumpCoords[currentAtom].type, inputDumpCoords[currentAtom].x, inputDumpCoords[currentAtom].y, inputDumpCoords[currentAtom].z);
+				// 	printf("%d %d %f %f %f\n", inputDumpCoords[currentAtom + 1].id, inputDumpCoords[currentAtom + 1].type, inputDumpCoords[currentAtom + 1].x, inputDumpCoords[currentAtom + 1].y, inputDumpCoords[currentAtom + 1].z);
+				// 	printf("%f %f %f\n", currentDimension.x, currentDimension.y, currentDimension.z);
+				// 	printf("\n\n");
+				// 	usleep (100000);
+				// }
+
+				if (currentDimension.x > maxDimension.x && currentDimension.x < simDimension.xLengthHalf) {
 					maxDimension.x = currentDimension.x; }
 
-				if (currentDimension.y > maxDimension.y) {
+				if (currentDimension.y > maxDimension.y && currentDimension.y < simDimension.yLengthHalf) {
 					maxDimension.y = currentDimension.y; }
 
-				if (currentDimension.z > maxDimension.z) {
+				if (currentDimension.z > maxDimension.z && currentDimension.z < simDimension.zLengthHalf) {
 					maxDimension.z = currentDimension.z; }
 
 				currentAtom += 2;
 			}
-			else
-			{
-				currentAtom++;
-			}
+			else {
+				currentAtom++; }
 		}
 
 		fprintf(stdout, "  x: %f; y: %f; z: %f (current timestep: %d)                               \r", maxDimension.x, maxDimension.y, maxDimension.z, currentTimestep);
