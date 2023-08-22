098562165275a7c312afab7d466df06c34b79691
Raghuram Elancheran <83778193+raghurame@users.noreply.github.com>
2023-08-23 00:43:32 +0530

    daily updates

--- avgBridgeCalculations/headers/computeBridgeCenterDistribution.h
+++ avgBridgeCalculations/headers/computeBridgeCenterDistribution.h
@@ -2,7 +2,7 @@
 #define BRIDGECENTERDISTRIBUTION_H
 
 BRIDGESBIN *assignBridgeCenterDistribution (BRIDGESBIN *bridgeCenterDistribution, int nBins_centerDistribution, float binWidth_centerDistribution, BOUNDARY simBoundary);
-BONDINFO *computeBridgeCenter (TRAJECTORY *atoms, int nAtoms, BONDINFO *allBonds, BOUNDARY simBoundary);
+BONDINFO *computeBridgeCenter (TRAJECTORY *atoms, int nAtoms, BONDINFO *allBonds, BOUNDARY simBoundary, FILE *file_bondDump);
 BRIDGESBIN *computeBridgeCenterDistribution (BONDINFO *allBonds, int nBonds, BRIDGESBIN *bridgeCenterDistribution, int nBins_centerDistribution);
 
 #endif
--- avgBridgeCalculations/headers/inputFunctions.h
+++ avgBridgeCalculations/headers/inputFunctions.h
@@ -3,7 +3,7 @@
 
 BOUNDARY getBoundary (FILE *file_inputTrj, BOUNDARY simBoundary);
 int countNAtoms (FILE *file_inputTrj);
-TRAJECTORY *getAtoms (TRAJECTORY *atoms, int nAtoms, BOUNDARY simBoundary, float distanceCutoff, FILE *file_inputTrj, int file_status, TRAJECTORY **micelles, int nMicelles);
+TRAJECTORY *getAtoms (TRAJECTORY *atoms, int nAtoms, BOUNDARY *simBoundary, float distanceCutoff, FILE *file_inputTrj, int file_status, TRAJECTORY **micelles, int nMicelles);
 int countNMicelles (FILE *file_inputTrj, int nAtoms);
 
 #endif
