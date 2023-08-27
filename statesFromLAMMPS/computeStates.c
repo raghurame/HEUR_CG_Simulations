3164ae50200cc5deff17e1c9af29f6a27a550051
Raghuram <raghurame@users.noreply.github.com>
2023-08-27 19:50:18 +0530

    daily updates

---
+++ statesFromLAMMPS/computeStates.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+	FILE
+	return 0;
+}
